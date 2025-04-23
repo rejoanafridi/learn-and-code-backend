56a91f20384e82e8afdd53102039afc0ebec4f25
rejoanafridi <rejoanafridi93@gmail.com>
2025-04-23 14:06:36 +0600

    fix(master): update in mongodb connector

--- configs/db.js
+++ configs/db.js
@@ -1,16 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error("MongoDB connection error:", error.message);
-    process.exit(1);
-  }
-};
+    try {
+        mongoose
+            .connect(process.env.MONGO_URI)
+            .then(() => console.log('MongoDB connected'))
+            .catch((err) => console.error('MongoDB connection error:', err))
 
-module.exports = connectDB;
+        console.log('MongoDB connected')
+    } catch (error) {
+        console.error('MongoDB connection error:', error.message)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
